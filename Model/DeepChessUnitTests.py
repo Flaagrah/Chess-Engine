7482d6acc3bad3d3e43b7bdd18279fec453a9ab9
Bhargava Guntoori <bguntoori@leanindustries.com>
2021-09-01 22:32:38 -0400

    Added more unit tests

--- Model/DeepChessUnitTests.py
+++ Model/DeepChessUnitTests.py
@@ -3,7 +3,27 @@ from Model import DeepChess
 import numpy as np
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_dataGeneratorTest(self):
+        white = np.load("sortedDataStr/whiteWins.npz")["states"]
+        whiteBoolStates = np.load("sortedDataStr/boolWhiteWins.npz")["bools"]
+        black = np.load("sortedDataStr/blackWins.npz")["states"]
+        blackBoolStates = np.load("sortedDataStr/boolBlackWins.npz")["bools"]
+
+        whiteWinData = []
+        blackWinData = []
+        for i in range(0, 100):
+            whiteWinData.append([white[i], whiteBoolStates[i]])
+            blackWinData.append([black[i], blackBoolStates[i]])
+
+        batch = 50
+        dataGen = DeepChess.DataGenerator(whiteWinData, blackWinData, batch)
+        self.assertEqual(2, dataGen.__len__())
+        item = dataGen.__getitem__(0)
+        self.assertEqual(batch, item[0][0].size/773)
+        self.assertEqual(batch, item[0][1].size/773)
+        self.assertEqual(batch, len(item[1]))
+
+    def test_boardToMatrix(self):
         mat = DeepChess.convertBoardToMatrix(
             [['.', 'k', '.', 'r', '.', '.', '.', '.'],
              ['.', 'p', '.', 'b', '.', 'n', '.', 'q'],
